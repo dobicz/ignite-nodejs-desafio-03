bcdf65f3fb3065727b721b87da8f8108955303a2
Paulo Henrique Peres <dobicz@gmail.com>
2021-03-16 19:05:27 -0300

    Commit

--- src/index.js
+++ src/index.js
@@ -8,29 +8,46 @@ app.use(express.json());
 
 const repositories = [];
 
+function addIdRepositoryToBody (request, response, next) {
+  const { id } = request.body;
+
+  if(!id) {
+    request.id = uuid();
+  }
+
+  return next();
+}
+
 app.get("/repositories", (request, response) => {
   return response.json(repositories);
 });
 
-app.post("/repositories", (request, response) => {
+app.post("/repositories", addIdRepositoryToBody, (request, response) => {
   const { title, url, techs } = request.body
 
-  const repository = {
-    id: uuid(),
+  const repository = {    
+    id: request.id,
     title,
     url,
     techs,
     likes: 0
   };
 
-  return response.json(repository);
+  repositories.push(repository);
+  
+  return response.status(201).json(repository);
 });
 
-app.put("/repositories/:id", (request, response) => {
+
+app.put("/repositories/:id", addIdRepositoryToBody, (request, response) => {
   const { id } = request.params;
   const updatedRepository = request.body;
 
-  repositoryIndex = repositories.findindex(repository => repository.id === id);
+  if (updatedRepository.likes) {
+    delete updatedRepository.likes;
+  }
+
+  repositoryIndex = repositories.findIndex(repository => repository.id === id);
 
   if (repositoryIndex < 0) {
     return response.status(404).json({ error: "Repository not found" });
@@ -45,10 +62,10 @@ app.put("/repositories/:id", (request, response) => {
 
 app.delete("/repositories/:id", (request, response) => {
   const { id } = request.params;
-
+  
   repositoryIndex = repositories.findIndex(repository => repository.id === id);
 
-  if (repositoryIndex > 0) {
+  if (repositoryIndex < 0) {
     return response.status(404).json({ error: "Repository not found" });
   }
 
@@ -68,7 +85,7 @@ app.post("/repositories/:id/like", (request, response) => {
 
   const likes = ++repositories[repositoryIndex].likes;
 
-  return response.json('likes');
+  return response.json({ likes });
 });
 
 module.exports = app;
